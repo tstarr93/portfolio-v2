b45708dc633c28a952916ed525f72185e225700b
Tim Starr <timstarr14@gmail.com>
2021-05-19 17:05:56 +0100

    Add svg styles

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import styles from "./Home.module.scss";
+import React from 'react';
+import styles from './Home.module.scss';
 
 const Home = () => {
   return (
     <>
-    <section className={styles.home}>
-      <img src="me-icon.svg" alt="tim starr" className={styles.icon}/>
-    </section>
+      <section className={styles.home}>
+        <div className={styles.image}>
+          {/* <img src="me-icon.svg" alt="tim starr" className={styles.icon} /> */}
+        </div>
+      </section>
     </>
   );
 };
